bf2b8d372c7917af994150e8e4308432d9cd7e88
h_forghani <forghani@arsh.co.ir>
2014-01-22 17:08:32 +0330

    logging minimum of distance.

--- match_2d_to_3d.m
+++ match_2d_to_3d.m
@@ -4,42 +4,41 @@ function matches = match_2d_to_3d(I, model)
 %%Extract features.
 query_im = single(rgb2gray(I));
 [sift_frames, sift_descriptors] = vl_sift(query_im);
+query_points_num = size(sift_frames, 2);
+fprintf('%d descriptors extracted.\n', query_points_num);
 
 %% Display features.
 figure;
 imshow(I);
 hold on;
 
-perm = randperm(size(sift_frames,2));
+perm = randperm(query_points_num);
 sel = perm(1:500);
 h1 = vl_plotframe(sift_frames(:,sel));
 h2 = vl_plotframe(sift_frames(:,sel));
 set(h1,'color','k','linewidth',3);
 set(h2,'color','y','linewidth',2);
 
+%% Register 2d to 3d.
 points_num = length(model.points);
-max_error = 7;
+max_error = 20;
 
 % Iterate on query image key points.
-for feature_index = 1:size(sift_frames, 2)
+for feature_index = 1:query_points_num
     query_f = sift_frames(:,feature_index);
     query_d = sift_descriptors(:,feature_index);
-%     query_meas = Measurement();
     point_pos = query_f(1:2,1);
-%     multiscale_des = query_meas.calc_desc_in_scales(query_im, point_pos);
-    fprintf('%i : query point (%f, %f):\n', feature_index, point_pos(1), point_pos(2));
 
     % Iterate on points.
     for point_index = 1:points_num
         pt = model.points{point_index};
 %         fprintf('%i : model point (%f, %f, %f) : ', point_index, pt.pos(1), pt.pos(2), pt.pos(3));
-
+        distances = zeros(pt.measure_num, 1);
         % Iterate on 3d point measurements.
         for measure_i = 1:pt.measure_num
             meas = pt.measurements{measure_i};
-%             [f, d, dist] = meas.multiscale_desc.get_best_match(multiscale_des);
             [f, d, dist] = meas.get_best_match(query_f, query_d);
-            
+            distances(measure_i) = dist;
 %             fprintf('%i:%f, ', meas.image_index, dist);
             if dist < max_error
                 fprintf('\n====== Matched: (%f, %f) to (%f, %f, %f) : %f ======\n', ...
@@ -48,6 +47,9 @@ for feature_index = 1:size(sift_frames, 2)
         end
 %         fprintf('\n');
     end
+    min_dist = min(distances);
+    fprintf('%i : query point (%f, %f) with min dist %f done.\n', ...
+        feature_index, point_pos(1), point_pos(2), min_dist);
 end
 
 matches = [];
